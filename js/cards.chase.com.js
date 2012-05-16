b23e564b9819ebd3a7598bd28286f09341bbcffd
Arthur Chiu <mr.arthur.chiu@gmail.com>
2012-05-16 08:51:00 -0700

    add dotjs

---
+++ js/cards.chase.com.js
@@ -0,0 +1,16 @@
+// Calculate the total activity cost
+function totalActivityCost() {
+  var total = 0;
+  $('.shim-td').each(function(i) {
+    total += parseFloat($(this).text().replace("$",""))
+  });
+  return total;
+}
+
+$(document).ready(function(){
+  setTimeout(function(){
+    // console.log("Activity Total Cost: $" + totalActivityCost());
+    var totalHtml = "<tr><td class=\"left\">Total Activity Cost:</td><td>$" + totalActivityCost().toFixed(2) + "</td></tr>"
+    $('table.card-accountdetailcol > tbody:first > tr:nth-child(3)').after(totalHtml);
+  }, 5000);
+});

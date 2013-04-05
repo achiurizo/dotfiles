34d3723d7f123487664aa63df34e1f5145048210
Arthur Chiu <mr.arthur.chiu@gmail.com>
2013-04-05 14:20:42 -0700

    set zsh

--- python/path.zsh
+++ python/path.zsh
@@ -2,5 +2,5 @@
 if which python > /dev/null; then
   export PYTHONSTARTUP=~/.pythonrc
   export PATH=/usr/local/share/python:$PATH
-  export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
+  export PYTHONPATH=/usr/local/lib/python2.7/site-packages
 fi

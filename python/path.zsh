013c80549afb4a516c77944f84f837fed2ab2949
Arthur Chiu <mr.arthur.chiu@gmail.com>
2013-04-07 06:50:29 -0700

    tweaks to make mac specific

--- python/path.zsh
+++ python/path.zsh
@@ -1,6 +1,8 @@
 # Python
 if which python > /dev/null; then
-  export PYTHONSTARTUP=~/.pythonrc
-  export PATH=/usr/local/share/python:$PATH
-  export PYTHONPATH=/usr/local/lib/python2.7/site-packages
+  if [ $(uname -s) = "Darwin" ]; then
+    export PYTHONSTARTUP=~/.pythonrc
+    export PATH=/usr/local/share/python:$PATH
+    export PYTHONPATH=/usr/local/lib/python2.7/site-packages
+  fi
 fi

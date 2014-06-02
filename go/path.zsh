753649ca40986d65f9ee2697059717e7ca71d8a9
Arthur Chiu <arthur.chiu@github.com>
2014-06-01 23:15:43 -0700

    remove language specific folders

--- go/path.zsh
+++
@@ -1,6 +0,0 @@
-# Go
-if which go > /dev/null; then
-  if [ $(uname -s) = "Darwin" ]; then
-    export GOPATH=$HOME/code/go
-  fi
-fi
--- python/path.zsh
+++
@@ -1,8 +0,0 @@
-# Python
-if which python > /dev/null; then
-  if [ $(uname -s) = "Darwin" ]; then
-    export PYTHONSTARTUP=~/.pythonrc
-    export PATH=/usr/local/share/python:$PATH
-    export PYTHONPATH=/usr/local/lib/python2.7/site-packages
-  fi
-fi

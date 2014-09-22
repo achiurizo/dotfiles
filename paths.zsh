efaa05e9ca86367ca172298590c9a2c45f4e8011
Arthur Chiu <arthur.chiu@github.com>
2014-09-22 00:53:52 -0700

    clean up language specific paths

--- paths.zsh
+++ paths.zsh
@@ -1,13 +1,9 @@
-# Paths
-#
-export PATH="~/bin:$HOME/.bin:/usr/local/bin:/usr/local/sbin:$PATH"
-
 # Go
 if which go > /dev/null; then
   if is_osx; then
     export GOPATH=$HOME/code/go
     export GOBIN=$HOME/code/go/bin
-    export PATH=$PATH:$(brew --prefix)/opt/go/libexec/bin
+    export PATH=$(brew --prefix)/opt/go/libexec/bin:$PATH
     export PATH=$GOBIN:$PATH
   fi
 fi
@@ -16,16 +12,14 @@ fi
 if which python > /dev/null; then
   if is_osx; then
     export PYTHONSTARTUP=$HOME/.pythonrc
-    export PATH=$(brew --prefix)/share/python:$PATH
     export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
   fi
 fi
 
 # Node
 if which npm > /dev/null; then
-  export PATH=/usr/local/share/npm/bin:$PATH
-  if [[ -e /opt/boxen/nodenv ]]
-  then
-    export NODE_PATH=$NODE_PATH:/opt/boxen/nodenv/versions/v0.10/lib/node_modules
+  if is_osx; then
+    export PATH=$(brew --prefix)/share/npm/bin:$PATH
+    export NODE_PATH=$(brew --prefix)/lib/node:$NODE_PATH
   fi
 fi

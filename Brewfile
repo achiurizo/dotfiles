f705155250098a17d2f8dbec9a75ca5b0fe1ea93
Arthur Chiu <achiurizo@Arthurs-MacBook-Pro.local>
2021-02-02 22:04:36 -0800

    back to osx, clean up fixes

--- Brewfile
+++ Brewfile
@@ -31,42 +31,5 @@ brew "pt"
 brew "jq"
 
 # gits
-brew "git", args: ["with-brewed-curl", "with-brewed-openssl"]
-brew "gist"
-brew "hub"
 brew "tig"
 brew "gnupg", args: ['with-readline']
-
-# javascript
-brew "node"
-brew "nvm"
-brew "yarn"
-
-# ruby
-brew "chruby"
-brew "chruby-fish"
-brew "ruby-build"
-brew "readline"
-
-# python
-brew "python"
-
-# go
-brew "go"
-brew "godep"
-
-# rust
-brew "rust"
-brew "rustup-init"
-brew "xsv"
-
-# security
-brew "hashpump"
-brew "nmap"
-brew "wireshark"
-
-# ethereum
-tap "paritytech/paritytech"
-brew "parity",  args: ['devel']
-
-cask "typora"
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,9 +8,29 @@ set -x PATH $PATH ~/.cargo/bin
 # Load the ASDF
 if test -e ~/.asdf/asdf.fish 
   source ~/.asdf/asdf.fish
+else
+  echo -e "\nsource "(brew --prefix asdf)"/asdf.fish" >> ~/.config/fish/config.fish
 end
 
 # Local config
 if test -e ~/.local.fish
   source ~/.local.fish
 end
+
+source /usr/local/opt/asdf/asdf.fish
+
+source /usr/local/opt/asdf/asdf.fish
+
+source /usr/local/opt/asdf/asdf.fish
+
+source /usr/local/opt/asdf/asdf.fish
+
+source /usr/local/opt/asdf/asdf.fish
+
+source /usr/local/opt/asdf/asdf.fish
+
+source /usr/local/opt/asdf/asdf.fish
+
+source /usr/local/opt/asdf/asdf.fish
+
+source /usr/local/opt/asdf/asdf.fish

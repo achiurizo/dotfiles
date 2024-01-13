5fd812e6e64c020d80b46ecdeb5fb29d6791ab0c
Arthur Chiu <achiurizo@users.noreply.github.com>
2024-01-13 12:46:26 +0800

    first pass init.lua

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,25 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = ","
+
+require("lazy").setup("plugins")
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+vim.api.nvim_set_keymap("n", "<leader>n", ":NvimTreeToggle<cr>", {silent = true, noremap = true})

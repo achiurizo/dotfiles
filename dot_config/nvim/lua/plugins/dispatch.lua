11bff4e9fc4a1a8d895674e71a6240cea65d84eb
Arthur Chiu <achiurizo@users.noreply.github.com>
2024-10-13 10:59:08 -0700

    update lazyvim

--- dot_config/nvim/lua/plugins/dispatch.lua
+++ dot_config/nvim/lua/plugins/dispatch.lua
@@ -2,7 +2,7 @@ return {
   [1] = "tpope/vim-dispatch",
   lazy = false,
   keys = {
-    { "<leader>di", "<Cmd>update | Dispatch<CR>" },
+    { "<leader>dd", "<Cmd>update | Dispatch<CR>", desc = "Save and Dispatch" },
   },
   config = function()
     vim.g.dispatch_quickfix_height = 20

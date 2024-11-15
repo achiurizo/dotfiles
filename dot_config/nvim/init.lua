24e6872178c45041dc59d4c8c987447cfcdfb503
Arthur Chiu <achiurizo@users.noreply.github.com>
2024-11-14 19:09:30 -0800

    add a custom prompt

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("CopilotChat").setup({
+  prompts = {
+    MiniTest = {
+      prompt = "/COPILOT_GENERATE please write tests in the minitest unit test style for this code.",
+    },
+  },
+})

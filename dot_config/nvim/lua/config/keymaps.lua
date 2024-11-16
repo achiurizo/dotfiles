6345ca9481fe8c69d78e23626a60ac36a7edf7d7
Arthur Chiu <achiurizo@users.noreply.github.com>
2024-11-15 22:41:21 -0800

    add copilot chat buffer input

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+local copilot_chat_buffer = function()
+    local input = vim.fn.input("Quick Chat: ")
+    if input ~= "" then
+      require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+    end
+  end
+
+map("n", "<leader>ab", copilot_chat_buffer, { desc = "CopilotChat - Quick chat" })

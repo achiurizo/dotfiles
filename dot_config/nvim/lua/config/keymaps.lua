0123db519267da8a07ec803f20751b1a2e63d28e
Arthur Chiu <achiurizo@users.noreply.github.com>
2024-11-27 22:30:08 -0800

    update plugins and add new keymaps

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -5,10 +5,19 @@
 local map = vim.keymap.set
 
 local copilot_chat_buffer = function()
-    local input = vim.fn.input("Quick Chat: ")
-    if input ~= "" then
-      require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
-    end
+  local input = vim.fn.input("Quick Chat On Buffer: ")
+  if input ~= "" then
+    require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
   end
+end
 
-map("n", "<leader>ab", copilot_chat_buffer, { desc = "CopilotChat - Quick chat" })
+map("n", "<leader>ab", copilot_chat_buffer, { desc = "CopilotChat - Quick chat on current buffer" })
+
+local copilot_chat_visual = function()
+  local input = vim.fn.input("Quick Chat On Visual: ")
+  if input ~= "" then
+    require("CopilotChat").ask(input, { selection = require("CopilotChat.select").visual })
+  end
+end
+
+map("v", "<leader>av", copilot_chat_visual, { desc = "CopilotChat - Quick chat on visual selection" })

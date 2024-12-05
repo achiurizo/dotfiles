f12a6213672b1dbd2603efb5d860ef528475e8d6
Arthur Chiu <achiurizo@users.noreply.github.com>
2024-12-05 12:58:23 -0800

    smear the cursor

---
+++ dot_config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {},
+    -- opts = {
+    --   cursor_color = "#ff8800",
+    --   stiffness = 0.3,
+    --   trailing_stiffness = 0.05,
+    --   trailing_exponent = 2,
+    --   volume_reduction_exponent = -0.1,
+    --   gamma = 1,
+    -- },
+  },
+}

b32717ed315e4b3e67993a7c7e43aa92600d541c
Arthur Chiu <achiurizo@users.noreply.github.com>
2024-12-12 17:21:10 -0800

    bump and add emmet/fix telescope conf

---
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    -- add a keymap to browse plugin files
+    -- stylua: ignore
+    -- {
+    --   "<leader>fp",
+    --   function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+    --   desc = "Find Plugin File",
+    -- },
+  },
+}

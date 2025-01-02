fb2e3c54c307fd6d1bc4d9fd75f3cc45f29c07e7
Arthur Chiu <achiurizo@users.noreply.github.com>
2025-01-01 19:49:33 -0800

    update packages, torte scheme

--- dot_config/nvim/lua/plugins/colorschemes.lua
+++ dot_config/nvim/lua/plugins/colorschemes.lua
@@ -12,9 +12,25 @@ return {
   "projekt0n/github-nvim-theme",
   "Biscuit-Theme/nvim",
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "biscuit",
-    },
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.zenbones_compat = 1
+      vim.cmd([[
+        highlight Normal guibg=NONE ctermbg=NONE
+        highlight NonText guibg=NONE ctermbg=NONE
+        colorscheme torte
+      ]])
+    end,
+    -- you can set set configuration options here
+    -- config = function()
+    --     vim.g.zenbones_darken_comments = 45
+    --     vim.cmd.colorscheme('zenbones')
+    -- end
   },
 }

984a592855c3c47dd5d8ade1a2fe7e0c578ca95a
Arthur Chiu <achiurizo@users.noreply.github.com>
2025-11-24 17:30:22 -0800

    resize symbols panel

---
+++ dot_config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/trouble.nvim",
+    opts = {
+      modes = {
+        symbols = {
+          win = {
+            type = "split",
+            relative = "win",
+            position = "right",
+            size = 50,
+          },
+        },
+      },
+    },
+  },
+}

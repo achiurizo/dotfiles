1f61e753127b8c1c0a88dcc24caac183647c4ebb
Arthur Chiu <achiurizo@users.noreply.github.com>
2025-11-26 11:38:44 -0800

    patch emmet

--- dot_config/nvim/lua/plugins/emmet.lua
+++ dot_config/nvim/lua/plugins/emmet.lua
@@ -1,3 +1,26 @@
 return {
-    "mattn/emmet-vim"
+    "mattn/emmet-vim",
+    init = function()
+        -- Patch emmet_utils to use modern vim.treesitter API instead of deprecated ts_utils
+        package.preload["emmet_utils"] = function()
+            local M = {}
+            M.get_node_at_cursor = function()
+                local node = vim.treesitter.get_node()
+                if not node then
+                    return nil
+                end
+                while node do
+                    local node_type = node:type()
+                    if node_type == "element" then
+                        return "html"
+                    elseif node_type == "stylesheet" then
+                        return "css"
+                    end
+                    node = node:parent()
+                end
+                return ""
+            end
+            return M
+        end
+    end,
 }

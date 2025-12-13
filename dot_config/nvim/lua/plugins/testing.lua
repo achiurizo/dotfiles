1e7408401a5564375012de0815cb00fec3aff457
Arthur Chiu <achiurizo@users.noreply.github.com>
2025-12-13 15:07:34 -0800

    update packages, add neotest ruby minitest

--- dot_config/nvim/lua/plugins/testing.lua
+++ dot_config/nvim/lua/plugins/testing.lua
@@ -4,11 +4,13 @@ return {
     dependencies = {
       "nvim-neotest/neotest-python",
       "marilari88/neotest-vitest",
+      "volodya-lombrozo/neotest-ruby-minitest",
     },
     opts = {
       adapters = {
         ["neotest-python"] = {},
         ["neotest-vitest"] = {},
+        ["neotest-ruby-minitest"] = {},
       },
     },
   },

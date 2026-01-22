44639694abde3b8491a434941c02b046c1c02b0c
achiurizo <24772+achiurizo@users.noreply.github.com>
2026-01-21 17:25:22 -0800

    add find/search shorthands for ignored

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Find files including ignored (.env, .gitignore, etc.)
+vim.keymap.set("n", "<leader>fI", function()
+  require("telescope.builtin").find_files({ hidden = true, no_ignore = true })
+end, { desc = "Find Files (incl. ignored)" })
+
+-- Grep including ignored files
+vim.keymap.set("n", "<leader>sI", function()
+  require("telescope.builtin").live_grep({
+    additional_args = { "--no-ignore", "--hidden" },
+  })
+end, { desc = "Grep (incl. ignored)" })
